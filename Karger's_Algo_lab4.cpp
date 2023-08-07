7080acdec28985cfd95f9c798dcfcd2d5ad5c4a9
Devanshuchhipani <96529796+Devanshuchhipani@users.noreply.github.com>
2023-08-07 10:15:21 +0530

    Add files via upload

--- Karger's_Algo_lab4.cpp
+++ Karger's_Algo_lab4.cpp
@@ -1,187 +1,157 @@
 #include <iostream>
-#include<bits/stdc++.h>
 using namespace std;
-#define n 4
+#define N 4
+int G[N][N]={{0,1,1,1},{1,0,0,1},{1,0,0,1},{1,1,1,0}};
+int G1[N][N];
 
-void karger_mincut(int G[n][n])
-{
+int main() {
+    // Write C++ code here
+    std::cout << "Hello world!";
     
     
-    int n1 = n;
-    
-    int G3[n1][n1]={0};
-    while( n1 > 2)
+    int i,j;int n=N;
+    while(n>2)
     {
-    //1
-    
-        int G1[n1][n1] = {0};
-        int u, v;
-        cout << "Enter Edge : ";
-        cin >> u >> v;
-        u--;
-        v--;
-        //2
-        for(int i = 0 ; i< n1  ; i++)
+    int u,v;
+    cout<<"Enter Edge:"<<endl;
+    cout<<"Enter U:"<<endl;
+    cin>>u;
+    cout<<"Enter V:"<<endl;
+    cin>>v;
+    int cnt=0;
+    for(i=0;i<n;i++)
+    {
+        for(j=i+1;j<n;j++)
         {
-            for(int j = i + 1 ; j< n1  ; j++)
+            cnt=0;
+            if(i==u && j==v)
+              G1[i][j]=G1[j][i]=0;
+            else if(i==u || i==v)
             {
-                int count =0;
-                        
-                if ( i == u && j ==v)
+                if(G[u][j]>0)
                 {
-                    G1[i][j] = G1[j][i]=0;
+                    cnt+=G[u][j];
                 }
-                else if ( i ==u || i ==v)
+                if(G[v][j]>0)
                 {
-                    if(G[u][j] > 0)
-                    {
-                        count += G[u][j];
-                    }
-                    if(G[v][j] > 0)
-                    {
-                        count += G[v][j];
-                    }
-                    if(u<j)
-                      G1[u][j]=count;
-                    else
-                      G1[j][u]=count;
-                    
-                    if(v<j)
-                      G1[v][j]=count;
-                    else
-                      G1[j][v]=count;
-                    
-                    //G1[u][j] = G1[v][j] = count;
+                    cnt+=G[v][j];
                 }
-                else if ( j == u || j ==v)
+                if(u<j)
                 {
-                    if(G[u][i] > 0)
-                    {
-                        count += G[u][i];
-                    }
-                    if(G[v][i] > 0)
-                    {
-                        count += G[v][i];
-                    }
-                    
-                    if(i<u)
-                      G1[i][u]=count;
-                    else
-                      G1[u][i]=count;
-                    
-                    if(i<v)
-                      G1[i][v]=count;
-                    else
-                      G1[v][i]=count;
-                    
-                //    G1[i][u] = G1[i][v] = count;
+                   G1[u][j]=cnt; 
                 }
                 else
                 {
-                    G1[i][j] = G[i][j];
+                    G1[j][u]=cnt;
                 }
-                
-                
-                //cout << G[i][j] << " ";
-            }
-            //cout << endl;
-        }
-        
-        //3
-        for(int i =0; i< n1 ; i++)
-        {
-            for(int j =0; j< n1 ; j++)
-            {
-                if( i>= j)
+                if(v<j)
                 {
-                    G1[i][j] = G1[j][i];
-                    
+                   G1[v][j]=cnt; 
                 }
+                else
+                {
+                    G1[j][v]=cnt;
+                }
+                //G1[u][j]=G1[v][j]=cnt;
             }
-        }    
-        
-        //4
-        int G2[n1-1][n1-1] = {0};
-        int r = 0 , c=0;
-        for(int i =0; i< n1 ; i++)
-        {
-            c=0;
-            if (i ==v)
-                continue;
-                
-            for(int j =0; j< n1 ; j++)
+            else if (j==u || j==v)
             {
-                if (j ==v )
-                    continue;
-                
-                G2[r][c] = G1[i][j];
-                c++;
-            
+                if(G[u][i]>0)
+                {
+                    cnt+=G[u][i];
+                }
+                if(G[v][i]>0)
+                {
+                    cnt+=G[v][i];
+                }
+                if(i<u)
+                {
+                    G1[i][u]=cnt;
+                }
+                else
+                {
+                    G1[u][i]=cnt;
+                }
+                if(i<v)
+                {
+                    G1[i][v]=cnt;
+                }
+                else
+                {
+                    G1[v][i]=cnt;
+                }
+                //G1[i][u]=G1[i][v]=cnt;
                 
             }
-            r++;
-        }
-        
-        // clear G
-        
-        for(int i =0; i< n ; i++)
-        {
-            for(int j =0; j< n ; j++)
+            else
             {
-               G[i][j] = 0;
-               G3[i][j] = G2[i][j];
+                G1[i][j]=G[i][j];
             }
         }
-        
-        //cout<<"g \n";
-        for(int i =0; i< n1-1 ; i++)
-        {
-            for(int j =0; j< n1-1 ; j++)
-            {
-                G[i][j] = G2[i][j];
-                //cout << G[i][j] << " " ;
-                
-            }
-            //cout << endl;
-        }
-        
-        n1 = n1 - 1;
-        
     }
-    
-    cout << "cut value : " << G[0][1]  << endl << "G : \n"; 
-    
-    for(int i =0; i< n ; i++)
+    for(i=0;i<n;i++)
     {
-        for(int j =0; j< n ; j++)
+        for(j=0;j<n;j++)
         {
-            cout << G[i][j] << " ";
+            cout<<G1[i][j]<<" ";
         }
-        cout << endl;
+        cout<<endl;
     }
-    cout << "G2 : " << endl;
-    for(int i =0; i< n1 ; i++)
+    for(i=0;i<n;i++)
     {
-        for(int j =0; j< n1 ; j++)
+        for(j=0;j<n;j++)
         {
-            cout << G3[i][j] << " ";
+            if(i>=j)
+            {
+                G1[i][j]=G1[j][i];
+            }
         }
-        cout << endl;
     }
-}
-
-
-int main()
-{
-    int G[n][n] = { {0,1,1,1},
-                    {1,0,0,1},
-                    {1,0,0,1},
-                    {1,1,1,0}
-                    };
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            cout<<G1[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    int x=n-1;
+    int G2[x][x];
+    int a=0,b=0;
+    for(i=0;i<n;i++)
+    {
+        if (i==v) continue;
+        b=0;
+        for(j=0;j<n;j++)
+        {
+            if (j==v) continue;
+            else
+            //if(i!=v and j!=v)
+            {
+                G2[a][b]=G1[i][j];
+                b++;
+            }
+        }
+        a++;
+    }
+    for(i=0;i<x;i++)
+    {
+        for(j=0;j<x;j++)
+        {
+          cout<<G2[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    for(i=0;i<x;i++)
+    {
+        for(j=0;j<x;j++)
+        {
+          G[i][j]=G2[i][j];
+        }
+    }
+    n=n-1;
+    }
     
-    karger_mincut(G);
-    
+    cout<<"Min-Cut="<<" "<<G[0][1];
     return 0;
 }
-
-
